b8a912f80f9e793dc9c9fbfd2feca98f29f929d2
Fae Hamilton-Raine <faehamiltonraine@gmail.com>
2025-02-20 22:26:36 -0700

    Finished with error handling

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::io::Error;
+
+fn extract_errors(text: &str) -> Vec<String> {
+    let split_text = text.split("\n");
+    let mut results = vec![];
+
+    for line in split_text {
+        if line.starts_with("ERROR") {
+            results.push(line.to_string());
+        }
+    }
+
+    results
+}
+
+fn main() -> Result<(), Error> {
+    let text = fs::read_to_string("logs.txt")?;
+    let error_logs = extract_errors(text.as_str());
+    fs::write("errors.txt", error_logs.join("\n"))?;
+
+    Ok(())
+
+    // let text = fs::read_to_string("logs.txt").expect("failed to read logs.txt");
+    // let error_logs = extract_errors(text.as_str());
+    // fs::write("errors.txt", error_logs.join("\n")).expect("failed to write errors.txt");
+
+    // match fs::read_to_string("logs.txt") {
+    //     Ok(text) => {
+    //         let error_logs = extract_errors(text.as_str());
+
+    //          match fs::write("errors.txt", error_logs.join("\n")) {
+    //             Ok(..) => println!("Wrote errors.txt"),
+    //             Err(fail_message) => println!("Writing failed: {}", fail_message)
+    //          }
+    //     }
+    //     Err(fail) => println!("Failed to read file: {}", fail)
+    // }
 }
